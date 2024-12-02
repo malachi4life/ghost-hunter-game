b3463442c340ba6912421b1eee830035ef46c1ca
John Devaney <johndevaney1@hotmail.com>
2024-12-02 19:29:56 +0000

    Added test enemy character and some basic movement
    Added an enemy character (triangle), and some basic random movement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,23 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector2 direction;
+    private float changeDirectionTime = 1f;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            timer = changeDirectionTime;
+        }
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
