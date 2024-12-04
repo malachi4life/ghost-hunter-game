1e074abd5c58f0118813a2894d22badbc0fe034c
John Devaney <johndevaney1@hotmail.com>
2024-12-04 20:29:58 +0000

    Sprite animation and animation flip
    Got player sprite to be animated and changed sprite scale when facing in different directions

--- Assets/Assets/Scripts/PlayerMovement.cs
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,47 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-
     private Vector2 movement;
-
-    // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-    }
+    public int facingDirection = 1; // 1 for right, -1 for left
+    public Rigidbody2D rb;
+    public Animator anim;
 
     void FixedUpdate()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Update animator parameters
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal)); // Use Mathf.Abs for positive values
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        // Check and flip the character's facing direction
+        if ((horizontal > 0 && transform.localScale.x < 0) || (horizontal < 0 && transform.localScale.x > 0))
+        {
+            Flip();
+        }
+
+        // Adjust speed for running
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
-            // Temporarily increase the speed
-            speed = 10f; // Adjust the boosted speed value as needed
+            speed = 10f; // Boosted speed
         }
         else
         {
-            // Reset to the default speed
-            speed = 5f; // Default speed value
+            speed = 5f; // Default speed
         }
-        transform.Translate(movement * speed * Time.deltaTime);
+
+        // Apply velocity
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    // Method to flip the character
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
